ca0b420cb5c7de0529245be1b1f02e2bd4f7602b
Ash Christopher <ash.christopher@gmail.com>
2011-07-15 12:43:43 -0700

    Url doesn't have to end in JSON - it is just an HTTP resource and should be treated as such.

--- ajax_resources/urls.py
+++ ajax_resources/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls.defaults import *
 
 urlpatterns = patterns('ajax_resources.views',
-    (r'^(?P<application>\w+)/(?P<resource>\w+).json', 'resource_loader'),
+    (r'^(?P<application>\w+)/(?P<resource>\w+)/$', 'resource_loader'),
 )
 
